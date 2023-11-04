d074b14b9e22306af05868e1c1b2b2ea860676aa
IO-oINik <nik.mokshin.2004@mail.ru>
2023-11-04 19:11:27 +0300

    add new InlineKeyboards

--- src/main/java/org/example/keyboardFactory/InlineKeyboardFactory.java
+++ src/main/java/org/example/keyboardFactory/InlineKeyboardFactory.java
@@ -1,5 +1,8 @@
 package org.example.keyboardFactory;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.example.model.Course;
+import org.example.model.Group;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -8,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InlineKeyboardFactory {
-    public static ReplyKeyboard allCourses() {
+    public static InlineKeyboardMarkup allCourses() {
         InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
@@ -25,14 +28,40 @@ public class InlineKeyboardFactory {
         }
         rowsInline.add(rowInline);
         rowInline = new ArrayList<>();
-        for(int i = 1; i <= 2; ++i) {
+        for(int i = 6; i <= 7; ++i) {
             InlineKeyboardButton button = new InlineKeyboardButton();
-            button.setText(i + " курс (Магистратура)");
-            button.setCallbackData("courseMag_" + i);
+            button.setText(i-5 + " курс (Магистратура)");
+            button.setCallbackData("course_" + i);
             rowInline.add(button);
         }
         rowsInline.add(rowInline);
         inlineKeyboard.setKeyboard(rowsInline);
         return inlineKeyboard;
+    }
+
+    public static InlineKeyboardMarkup allGroupsForCourse(Course course) {
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        int i = 0;
+        List<Group> groups = course.getGroups();
+        for (Group group: groups) {
+            ++i;
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(Integer.toString(group.getNumber()));
+            button.setCallbackData("group_" + group.getNumber() + "_course_" + course.getNumber());
+            rowInline.add(button);
+            if(i == 3) {
+                rowsInline.add(rowInline);
+                rowInline = new ArrayList<>();
+            }
+        }
+        rowsInline.add(rowInline);
+        inlineKeyboard.setKeyboard(rowsInline);
+        return inlineKeyboard;
+    }
+
+    public static InlineKeyboardMarkup empty() {
+        return new InlineKeyboardMarkup();
     }
 }
