538aa48f17d7c3ece402ce42061c32375a991a55
IO-oINik <nik.mokshin.2004@mail.ru>
2023-11-05 05:03:08 +0300

    added functions for displaying the schedule for today or now

--- src/main/java/org/example/bot/ScheduleBot.java
+++ src/main/java/org/example/bot/ScheduleBot.java
@@ -4,9 +4,13 @@ import org.example.keyboardFactory.InlineKeyboardFactory;
 import org.example.config.Config;
 import org.example.constant.Constants;
 import org.example.model.Course;
-import org.example.model.Group;
+import org.example.model.Subject;
+import org.example.model.User;
 import org.example.service.CourseService;
+import org.example.service.SubjectService;
 import org.example.service.UserService;
+import org.example.util.DateUtil;
+import org.example.util.MessageUtils;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.bot.BaseAbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
@@ -14,12 +18,12 @@ import org.telegram.abilitybots.api.objects.Flag;
 import org.telegram.abilitybots.api.objects.Reply;
 import org.telegram.abilitybots.api.util.AbilityUtils;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
@@ -31,11 +35,13 @@ public class ScheduleBot extends AbilityBot implements Constants {
 
     private final UserService userService;
     private final CourseService courseService;
+    private final SubjectService subjectService;
 
     public ScheduleBot() {
         super(Config.getToken(), Config.getName());
         userService = new UserService();
         courseService = new CourseService();
+        subjectService = new SubjectService();
     }
 
     @Override
@@ -43,6 +49,11 @@ public class ScheduleBot extends AbilityBot implements Constants {
         return 878758046;
     }
 
+    /**
+     * Функция вызываеться по команде /start<br>
+     * Выводит приветственное сообщение бота
+     * @return
+     */
     public Ability start() {
         return Ability
                 .builder()
@@ -54,6 +65,11 @@ public class ScheduleBot extends AbilityBot implements Constants {
                 .build();
     }
 
+    /**
+     * Функция вызываеться по команде /reg<br>
+     * Выводит клавиатуру содержащуюю все курсы
+     * @return
+     */
     public Ability start_registration() {
         return Ability
                 .builder()
@@ -71,6 +87,11 @@ public class ScheduleBot extends AbilityBot implements Constants {
                 .build();
     }
 
+    /**
+     * Функция вызваеться при нажатии на клавиатуру с выбором курса<br>
+     * Меняет описание и клавиатуру сообщения на выбор группы
+     * @return
+     */
     public Reply chooseGrope() {
         BiConsumer<BaseAbilityBot, Update> action = (bot, upd) -> {
             int courseNumber = Integer.parseInt(upd.getCallbackQuery().getData().split("_")[1]);
@@ -88,10 +109,19 @@ public class ScheduleBot extends AbilityBot implements Constants {
         return Reply.of(action, Flag.CALLBACK_QUERY, isPressInlineKeyboardAllCourses());
     }
 
+    /**
+     * Функция-условие определяет нажатие на клавиатуру с выбором курса
+     * @return
+     */
     private Predicate<Update> isPressInlineKeyboardAllCourses() {
         return upd -> upd.hasCallbackQuery() && upd.getCallbackQuery().getData().startsWith("course");
     }
 
+    /**
+     * Функция вызываеться при нажатии на клавиатуру с выбором группы<br>
+     * Добаваляет или изменяет запись в базе данных о пользователе
+     * @return
+     */
     public Reply addOrUpdateUserInDB() {
         BiConsumer<BaseAbilityBot, Update> action = (bot, upd) -> {
             String[] data = upd.getCallbackQuery().getData().split("_");
@@ -108,7 +138,88 @@ public class ScheduleBot extends AbilityBot implements Constants {
         return Reply.of(action, Flag.CALLBACK_QUERY, isPressInlineKeyboardAllGroupsForCourse());
     }
 
+    /**
+     * Функция-условие определяет нажатие на клавиатуру с выбором группы
+     */
     private Predicate<Update> isPressInlineKeyboardAllGroupsForCourse() {
         return upd -> upd.hasCallbackQuery() && upd.getCallbackQuery().getData().startsWith("group");
+    }
+
+    /**
+     * Функция вызывается по команде /today<br>
+     * Выводит расписание на сегодня
+     */
+    public Ability now() {
+        return Ability
+                .builder()
+                .name("now")
+                .info(NOW_INFO)
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(ctx.chatId());
+                    sendMessage.setParseMode("HTML");
+                    User user = userService.findUserByTelegramId(ctx.user().getId());
+                    if(user == null) {
+                        sendMessage.setText(UNREGISTER_MESSAGE);
+                    } else {
+                        LocalDateTime localDateTime = LocalDateTime.now();
+                        String header = MessageUtils.header(user, localDateTime);
+                        String body;
+                        List<Subject> subjects = subjectService.findByCourseAndGroupAndDateAndIsNumerator_NOW(user.getCourse(), user.getGroup(), localDateTime, DateUtil.isNumerator(localDateTime.toLocalDate()));
+                        if(subjects.isEmpty()) {
+                            body = NO_SUBJECTS_MESSAGE;
+                        } else {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for (Subject subject: subjects) {
+                                stringBuilder.append(MessageUtils.subject(subject));
+                            }
+                            body = stringBuilder.toString();
+                        }
+                        sendMessage.setText(header +  body);
+                    }
+                    silent.execute(sendMessage);
+                })
+                .build();
+    }
+
+    /**
+     * Функция вызывается по команде /week<br>
+     * Выводит расписание на неделю
+     */
+    public Ability today() {
+        return Ability
+                .builder()
+                .name("today")
+                .info(TODAY_INFO)
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(ctx.chatId());
+                    sendMessage.setParseMode("HTML");
+                    User user = userService.findUserByTelegramId(ctx.user().getId());
+                    if(user == null) {
+                        sendMessage.setText(UNREGISTER_MESSAGE);
+                    } else {
+                        LocalDateTime localDateTime = LocalDateTime.now();
+                        String header = MessageUtils.header(user, localDateTime);
+                        String body;
+                        List<Subject> subjects = subjectService.findByCourseAndGroupAndDateAndIsNumerator_Day(user.getCourse(), user.getGroup(), localDateTime, DateUtil.isNumerator(localDateTime.toLocalDate()));
+                        if(subjects.isEmpty()) {
+                            body = NO_SUBJECTS_MESSAGE;
+                        } else {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for (Subject subject: subjects) {
+                                stringBuilder.append(MessageUtils.subject(subject));
+                            }
+                            body = stringBuilder.toString();
+                        }
+                        sendMessage.setText(header +  body);
+                    }
+                    silent.execute(sendMessage);
+                })
+                .build();
     }
 }
--- src/main/java/org/example/constant/Constants.java
+++ src/main/java/org/example/constant/Constants.java
@@ -1,5 +1,8 @@
 package org.example.constant;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface Constants {
     String START_INFO = "Старт";
     String START_MESSAGE = "Привет!\nЭтот бот создан для студентов ВГУ факультета ПММ.";
@@ -8,4 +11,9 @@ public interface Constants {
     String CHOOSE_COURSE_MESSAGE = "Выберите курс";
 
     String SUCCESSFUL_REGISTRATION = "Успешная регистрация!";
+    String TODAY_INFO = "Расписание на сегодня";
+    String NOW_INFO = "Текущий предмет";
+    String UNREGISTER_MESSAGE = "Вы не зарегистрированы.\nВведите команду: /reg";
+    String NO_SUBJECTS_MESSAGE = "На сегодня занятий не наблюдается...\n Отличный день для отдыха!";
+    String WEEK_INFO = "Расписание на неделю";
 }
